aa85101df32b582446a41db091120602673c35aa
Stevan Dedovic <stevan@dedovic.com>
2015-09-30 14:00:18 -0500

    setup couchdb on startup

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ var express = require('express');
 var app = express(); 
 var bodyParser = require('body-parser'); 
 var morgan = require('morgan');
-
 var config = require('./config');
+var nano = require('nano')(config.couchdb);
 
 
 // APP CONFIGURATION --------------------
@@ -14,12 +14,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // configure our app to handle CORS requests
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-	next();
+// app.use(function(req, res, next) {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+// 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+// 	next();
+// });
+
+// setup db if need be
+nano.db.create('users', function(err, body){
+	// this lists users by email. used to search the db for logging in
+	nano.db.use('users').insert({
+		_id: '_design/users',
+		_rev: '1-8c6878eaf5e7713e85ee57f2639c6954',
+		language: 'javascript',
+		views: {
+			email:{
+				map: function(doc) {
+					if(doc.email && doc.password)
+						emit(doc.email, doc.password);
+				}
+			}
+		}
+	});
 });
+nano.db.create('images');
+nano.db.create('jive');
 
 // log all requests to the console
 app.use(morgan('dev'));
